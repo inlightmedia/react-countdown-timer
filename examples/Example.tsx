d2d964a988fb414ba86ab615ec28b2b7dd3fbd20
Josh Dyck <josh@cyno.ca>
2020-07-25 21:33:00 -0400

    Add Support for daysand optional separator and time units

--- examples/Example.tsx
+++ examples/Example.tsx
@@ -5,7 +5,12 @@ import CountDownTimer from '../src/index';
 const Example = () => {
   return (
     <div style={{ backgroundColor:'rgb(50,50,50)', fontSize: '4em', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-      <CountDownTimer shouldShowSeparator={false} shouldShowTimeUnits={true} elapsedTimeColor="rgb(255, 0, 0)" countDownDateTime="2020-07-25T23:19:19Z" />
+      <CountDownTimer 
+        shouldShowSeparator={false}
+        shouldShowTimeUnits={true} 
+        elapsedTimeColor="rgb(255, 0, 0)" 
+        countDownDateTime="2020-08-25T23:19:19Z" 
+      />
     </div>
   );
 }
